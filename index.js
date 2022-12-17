60beb73db89ad71062bf0935b6b872a29013968d
Matheus Nascimento <94863819+matheusnsilva7@users.noreply.github.com>
2022-12-17 05:00:16 -0300

    Update index.js

--- index.js
+++ index.js
@@ -97,7 +97,7 @@ menuCarinho.addEventListener("click", function () {
 });
 
 barraDePesquisa.addEventListener("keyup", function () {
-  const palavra = barraDePesquisa.value.toLowerCase().replace(" ", "");
+  const palavra = barraDePesquisa.value.toLowerCase().replaceAll(" ", "");
   const barraProdutos = produtosLoja.items.filter((produtos) => {
     return produtos.product.name
       .replace(/[ `~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]/gi, "")
